498966e5ff34417c940e4c6197f7b6093b1476d3
imguozr <imguozr@gmail.com>
2019-03-18 22:01:36 +0800

    Simplify methods

--- spider.py
+++ spider.py
@@ -12,12 +12,57 @@ HEADERS = {
 }
 
 
-def get_category_page(category):
-    url = 'http://202.119.228.6:8080/browse/cls_browsing_book.php'
-    data = {
-        's_doctype': 'all',
-        'cls': category
+def get_book_html(book_id):
+    url = 'http://202.119.228.6:8080/opac/item.php'
+    book_data = {
+        'marc_no': str(book_id)
     }
+    book_html = requests.get(url, headers=HEADERS, params=book_data)
+    book_html.encoding = 'utf-8'
+    return book_html.text
+
+
+def get_book_attributions(book_id):
+    book_soup = BeautifulSoup(get_book_html(book_id), 'lxml')
+    items = book_soup.find_all(class_='booklist')
+    if items[0].get_text() == '':
+        print('无馆藏！')
+        return None
+    else:
+        attr_list = []
+        # try:
+        attr_dict = {'id': book_id}
+        for item in items:
+            text = item.get_text()
+            if text == '':
+                continue
+            if text[0] == '\n':
+                attr_list.append(text[1:-1].split('\n'))
+            else:
+                attr = text.split('\n')
+                attr.append('')
+                attr_list.append(attr)
+        for attr in attr_list:
+            attr_dict[attr[0]] = attr[1]
+        return attr_dict
+
+
+def get_page(category='ALL', day=3, book_type='ALL', location='ALL'):
+    if category != 'ALL':
+        url = 'http://202.119.228.6:8080/browse/cls_browsing_book.php'
+        data = {
+            's_doctype': 'all',
+            'cls': category
+        }
+    else:
+        url = 'http://202.119.228.6:8080/newbook/newbook_cls_book.php'
+        # 总馆00 密集书库 01
+        data = {
+            'back_days': str(day),
+            's_doctype': book_type,
+            'cls': category,
+            'loca_code': location
+        }
 
     html = requests.get(url, headers=HEADERS, params=data)
     html.encoding = 'utf-8'
@@ -29,7 +74,7 @@ def get_category_page(category):
         return 1
 
 
-def get_book_ids(category, page):
+def get_book_ids_in_category(category, page):
     print(page)
     url = 'http://202.119.228.6:8080/browse/cls_browsing_book.php'
     data = {
@@ -46,49 +91,10 @@ def get_book_ids(category, page):
     return re.findall(r'\d{10}', str(items))
 
 
-def get_all_books(category):
-    book_ids = []
-    page = get_category_page(category)
-    for i in range(page):
-        print(i + 1)
-        book_ids += get_book_ids(category, i + 1)
-    return book_ids
-
-
-def get_book_html(book_id):
-    url = 'http://202.119.228.6:8080/opac/item.php'
-    book_data = {
-        'marc_no': str(book_id)
-    }
-    book_html = requests.get(url, headers=HEADERS, params=book_data)
-    book_html.encoding = 'utf-8'
-    return book_html.text
-
-
-def get_book_attributions(book_id):
-    book_soup = BeautifulSoup(get_book_html(book_id), 'lxml')
-    items = book_soup.find_all(class_='booklist')
-    attr_list = []
-    attr_dict = {'id': book_id}
-    for item in items:
-        text = item.get_text()
-        if text == '':
-            continue
-        if text[0] == '\n':
-            attr_list.append(text[1:-1].split('\n'))
-        else:
-            attr = text.split('\n')
-            attr.append('')
-            attr_list.append(attr)
-    for attr in attr_list:
-        attr_dict[attr[0]] = attr[1]
-    return attr_dict
-
-
-def get_book_ids_in_category(cate):
+def get_all_book_ids_in_category(cate):
     pool = Pool(64)
-    page = get_category_page(cate)
-    get = partial(get_book_ids, cate)
+    page = get_page(category=cate)
+    get = partial(get_book_ids_in_category, cate)
     raw_data = pool.map(get, range(1, page + 1))
     pool.close()
     pool.join()
@@ -104,13 +110,12 @@ def get_book_ids_in_category(cate):
     return result
 
 
-def get_latest_book_ids(day=3, category='ALL', type='ALL', location='ALL'):
-
+def get_latest_book_ids(day=3, category='ALL', book_type='ALL', location='ALL'):
     url = 'http://202.119.228.6:8080/newbook/newbook_cls_book.php'
     # 总馆00 密集书库 01
     data = {
         'back_days': str(day),
-        's_doctype': type,
+        's_doctype': book_type,
         'cls': category,
         'loca_code': location
     }
@@ -124,7 +129,7 @@ def get_latest_book_ids(day=3, category='ALL', type='ALL', location='ALL'):
 
 if __name__ == '__main__':
     ts = time()
-    # start_pool()
-    # get_book_ids_in_category('B8')
-    print(get_latest_book_ids())
+    print(get_book_attributions('0000812471'))
+    # get_all_book_ids_in_category('A')
+    # print(get_page(category='A'))
     print(time() - ts)
