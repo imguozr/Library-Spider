f14d467ab1c38aebdfe4bb2b1983fe8474f77dec
imguozr <imguozr@gmail.com>
2019-04-06 18:10:07 +0800

    Update book item

--- library_spider/items.py
+++ library_spider/items.py
@@ -12,15 +12,17 @@ class BookItem(scrapy.Item):
     id = scrapy.Field()
     title = scrapy.Field()
     author = scrapy.Field()
+    sub_author = scrapy.Field()  # 个人次要责任者
+    carrier = scrapy.Field()  # 载体形态项
     publisher = scrapy.Field()
     isbn = scrapy.Field()
     price = scrapy.Field()
     cluster_item = scrapy.Field()  # 丛编项
-    subject = scrapy.Field()
-    category = scrapy.Field()
+    subject = scrapy.Field()  # 学科主题
+    category = scrapy.Field()  # 中图法分类号
     author_notes = scrapy.Field()  # 题名责任附注
+    publisher_notes = scrapy.Field()  # 出版发行附注
     version_notes = scrapy.Field()  # 版本附注
-    Bibliographic_notes = scrapy.Field()  # 书目附注
+    bibliographic_notes = scrapy.Field()  # 书目附注
     abstract = scrapy.Field()  # 提要文摘附注
     douban_description = scrapy.Field()  # 豆瓣简介
-
--- library_spider/spiders/book.py
+++ library_spider/spiders/book.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 import scrapy
 
 from library_spider.items import BookItem
@@ -7,17 +8,81 @@ from library_spider.items import BookItem
 class BookSpider(scrapy.Spider):
     name = 'book'
     allowed_domains = ['202.119.228.6:8080']
-    # start_urls = ['http://202.119.228.6:8080/opac/item.php?marc_no=' + '%10d' % id
-    #               for id in range(1, 10000000)]
-    start_urls = ['http://202.119.228.6:8080/opac/item.php?marc_no=0000802345']
+    start_urls = ['http://202.119.228.6:8080/opac/item.php?marc_no=' + '%010d' % id
+                  for id in range(1, 10000000)]
+    # start_urls = ['http://202.119.228.6:8080/opac/item.php?marc_no=0000805345']
 
     def parse(self, response):
-        items = []
+        book_dict = {}
+
+        book_item = BookItem()
 
         for each in response.xpath('//dl[@class="booklist"]'):
-            item = BookItem()
-            key = each.xpath('dt/text()').extract()
-            value = each.xpath('dd/a/text()').extract() + each.xpath('dd/text()').extract()
-            print(key, value)
-            # pass
+            # 属性名
+            try:
+                text = each.xpath('dt/text()').extract()[0]
+                if '豆瓣' not in text:
+                    key = text.split(':')[0]
+                else:
+                    key = text.split('：')[0]
+            except IndexError:
+                continue
+            # 属性值
+            try:
+                value = each.xpath('dd/a/text()').extract()[0] + each.xpath('dd/text()').extract()[0]
+            except IndexError:
+                value = each.xpath('dd/text()').extract_first() or each.xpath('dd/a/text()').extract_first()
+            if not value:
+                continue
 
+            if key in book_dict:
+                if isinstance(book_dict.get(key), list):
+                    book_dict[key] = book_dict.get(key) + [value]
+                else:
+                    book_dict[key] = [book_dict.get(key)] + [value]
+            else:
+                if key == '题名/责任者':
+                    book_dict['题名'] = value.split('/')[0]
+                elif key == 'ISBN及定价':
+                    book_dict['ISBN'] = value.split('/')[0]
+                    book_dict['定价'] = value.split('/')[1]
+                else:
+                    book_dict[key] = value
+
+        print(book_dict)
+
+        for key in book_dict:
+            if key == '题名':
+                book_item['title'] = book_dict[key]
+            elif key == '出版发行项':
+                book_item['publisher'] = book_dict[key]
+            elif key == '个人责任者':
+                book_item['author'] = book_dict[key]
+            elif key == '个人次要责任者':
+                book_item['sub_author'] = book_dict[key]
+            elif key == 'ISBN':
+                book_item['isbn'] = book_dict[key]
+            elif key == '定价':
+                book_item['price'] = book_dict[key]
+            elif key == '载体形态项':
+                book_item['carrier'] = book_dict[key]
+            elif key == '丛编项':
+                book_item['cluster_item'] = book_dict[key]
+            elif key == '学科主题':
+                book_item['subject'] = book_dict[key]
+            elif key == '中图法分类号':
+                book_item['category'] = book_dict[key]
+            elif key == '题名责任附注':
+                book_item['author_notes'] = book_dict[key]
+            elif key == '版本附注':
+                book_item['version_notes'] = book_dict[key]
+            elif key == '出版发行附注':
+                book_item['publisher_notes'] = book_dict[key]
+            elif key == '书目附注':
+                book_item['bibliographic_notes'] = book_dict[key]
+            elif key == '提要文摘附注':
+                book_item['abstract'] = book_dict[key]
+            elif key == '豆瓣简介':
+                book_item['douban_description'] = book_dict[key]
+
+        return book_item
